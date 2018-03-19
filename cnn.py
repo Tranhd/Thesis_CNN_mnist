1e5c6ff1e2e05ac79a41c73d4315dfe6535bd58f
masterdeepface <skrapmaillt@gmail.om>
2018-03-19 11:14:43 +0100

    Add documentation

--- cnn.py
+++ cnn.py
@@ -1,69 +1,40 @@
+# model.py
+# The CNN-classifier to be supervised on the Mnist dataset
+
+
+# Imports
 import tensorflow as tf
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 
 
 class MnistCNN(object):
-    def __init__(self, sess, save_dir='MnistCNN_save/', log_dir='logs/'):
-        self.sess = sess
-        self.build_model()
+    def __init__(self, sess, save_dir='./MnistCNN_save/'):
+        """
+        Init-function of the Mnist CNN class
+
+        :param sess: Tensorflow session
+        :param save_dir: String
+            Save directory for the graph
+        """
+        self.sess = sess  # Assign Tensorflow session to model.
         self.save_dir = save_dir
-        self.log_dir = log_dir
-
-    def build_model(self):
-        with tf.variable_scope('Placeholders'):
-            self.inputs = tf.placeholder(tf.float32, [None, 28, 28, 1])
-            self.labels = tf.placeholder(tf.int16, [None, 10])
-            self.training = tf.placeholder(tf.bool)
-            self.learning_rate = tf.placeholder(tf.float32)
-            self.activations = list()
-            self.predictions, self.logits = self.network(self.inputs)
-            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=self.labels, logits=self.logits)
-            self.loss = tf.reduce_mean(cross_entropy)
-            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
-            self.saver = tf.train.Saver()
-
-
-    def train_model(self, x_train, y_train, x_val, y_val, batch_size=64, epochs=100, learning_rate=1e-2, verbose=1):
-        N = len(x_train) // batch_size
-        try:
-            self.saver.restore(self.sess, tf.train.latest_checkpoint(self.save_dir))
-        except:
-            self.sess.run(tf.global_variables_initializer())
-        print('Starting training ...')
-        for epoch in range(epochs):
-            idx = np.random.permutation(len(x_train))
-            x, y = x_train[idx], y_train[idx]
-            if verbose: print('='*30 + f' Epoch {epoch+1} ' + '='*30)
-            loss = 0
-            batch_start = 0
-            batch_end = batch_size
-            for i in range(N):
-                if batch_end <= len(x):
-                    x_batch, y_batch = x[batch_start:batch_end, :, :, :], y[batch_start:batch_end, :]
-                    _, loss_ = self.sess.run([self.optimizer, self.loss],
-                                             feed_dict = {self.inputs: x_batch, self.labels: y_batch, self.learning_rate: learning_rate,
-                                                          self.training: True})
-                    loss = loss + loss_
-                    batch_start = batch_end
-                    batch_end = batch_end + batch_size
-            if verbose: print(f'Average Training loss {loss/N}')
-            validation_loss = self.loss.eval(session=self.sess, feed_dict={self.inputs: x_val, self.labels: y_val, self.training: False})
-            if verbose: print(f'Validation loss {validation_loss}')
-            self.saver.save(sess, save_path=self.save_dir + 'Cnn_mnist.ckpt')
-
-    def predict(self, test_image):
-        try:
-            self.saver.restore(self.sess, tf.train.latest_checkpoint(self.save_dir))
-        except:
-            raise Exception(f'Train the model before testing, cant find checkpoint in {self.save_dir}')
-
-        probs, activations = self.sess.run([self.predictions, self.activations], feed_dict={self.inputs: test_image, self.training: False})
-
-        predictions = np.argmax(probs, axis=1)
-        return predictions, probs, activations
+        self.build_model()  # Build the graph.
 
     def network(self, input):
+        """
+        Predicts the class of test_image
+
+        :param input: numpy array
+            The test_images to be classified [n_examples, 28, 28 ,1]
+
+        :return predictions: numpy array
+            The predicted class for every example in test_image [n_examples, 1]:
+        :return probs: numpy array
+            Probability distribution for the predictions [n_examples, 10]
+
+        :raises: Exception
+            When model has no checkpoint and weights to load.
+        """
         with tf.variable_scope('Network'):
 
             # Convolutional Layer #1
@@ -100,252 +71,141 @@ class MnistCNN(object):
             predictions = tf.nn.softmax(logits)
             return predictions, logits
 
+    def build_model(self):
+        """
+        Builds the Tensorflow graph
+
+        """
+        # Placeholders
+        with tf.variable_scope('Placeholders'):
+            self.inputs = tf.placeholder(tf.float32, [None, 28, 28, 1])  # Mnist input.
+            self.labels = tf.placeholder(tf.int16, [None, 10])  # Labels, one-hot encoded.
+            self.training = tf.placeholder(tf.bool)  # Bool indicating if in training mode.
+            self.learning_rate = tf.placeholder(tf.float32)  # Learning rate.
+
+        # Activations from CNN
+        with tf.variable_scope('Activations'):
+            self.activations = list()
+
+        # Predictions and Logits
+        with tf.variable_scope('Predictions'):
+            self.predictions, self.logits = self.network(self.inputs)  # Builds network
+
+        # Loss.
+        with tf.variable_scope('Loss'):
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=self.labels, logits=self.logits)  # Cross entropy loss
+            self.loss = tf.reduce_mean(cross_entropy)  # Loss
+
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).minimize(self.loss)  # Optimizer
+
+        self.saver = tf.train.Saver()  # Saver
+
+    def train_model(self, x_train, y_train, x_val, y_val, batch_size=64, epochs=100, learning_rate=1e-2, verbose=1):
+        """
+        Trains the model
+
+        :param x_train: numpy array
+            Training set input data [batch_size, 28, 28, 1]
+        :param y_train: y_train : numpy array
+            Training set labels [batch_size, 10] (one-hot encoded)
+        :param x_val: x_val : numpy array
+            Validation set input data [batch_size, 28, 28, 1]
+        :param y_val: y_val : numpy array
+            Validation set labels [batch_size, 10] (one-hot encoded)
+        :param batch_size: batch_size (optional): int
+            Batch size
+        :param epochs: epochs (optional): int
+            Epochs to run
+        :param learning_rate: learning_rate (optional): float
+            Learning rate
+        :param verbose: verbose (optional): binary 0 or 1
+            Specifies level of Info
+        """
+        N = len(x_train) // batch_size # Number of iterations per epoch
+        try:
+            self.saver.restore(self.sess, tf.train.latest_checkpoint(self.save_dir))  # Restore if checkpoint exists.
+        except:
+            self.sess.run(tf.global_variables_initializer())  # Otherwise initialize.
+        print('Starting training ...')
+        for epoch in range(epochs):
+            idx = np.random.permutation(len(x_train))
+            x, y = x_train[idx], y_train[idx]  # Shuffle training data.
+            if verbose:
+                print('='*30 + f' Epoch {epoch+1} ' + '='*30)
+            loss = 0
+            batch_start = 0
+            batch_end = batch_size
+            for i in range(N):
+                if batch_end <= len(x):
+                    x_batch, y_batch = x[batch_start:batch_end, :, :, :], y[batch_start:batch_end, :]  # Create batch.
+
+                    # Optimize parameters for batch.
+                    _, loss_ = self.sess.run([self.optimizer, self.loss],
+                                             feed_dict={self.inputs: x_batch, self.labels: y_batch,
+                                                        self.learning_rate: learning_rate, self.training: True})
+                    loss = loss + loss_  # Add to total epoch loss.
+                    batch_start = batch_end  # Next batch.
+                    batch_end = batch_end + batch_size
+            if verbose:
+                print(f'Average Training loss {loss/N}')  # Print average training loss for epoch.
+
+            # Evaluate on validation set.
+            validation_loss = self.loss.eval(session=self.sess,
+                                             feed_dict={self.inputs: x_val, self.labels: y_val, self.training: False})
+            if verbose:
+                print(f'Validation loss {validation_loss}')  # Print validation loss for epoch
+        self.saver.save(self.sess, save_path=self.save_dir + 'Cnn_mnist.ckpt')  # Save parameters.
+
+    def predict(self, test_image):
+        """
+        Predicts the class of test_image
+
+        :param test_image: numpy array
+            The test_images to be classified [n_examples, 28, 28 ,1]
+
+        :return predictions: numpy array
+            The predicted class for every example in test_image [n_examples, 1]
+        :return probs: numpy array
+            Probability distribution for the predictions [n_examples, 10]
+        :return activations : list
+            List with activations from each layer of the network, entry i contains
+            activations from layer i, numpy array of shape [n_examples, feature map size of layer i]
+
+        :raises Exception
+            When model has no checkpoint and weights to load.
+        """
+        try:
+            self.saver.restore(self.sess, tf.train.latest_checkpoint(self.save_dir))  # Try to restore weights.
+        except:
+            raise Exception(f'Train the model before testing, cant find checkpoint in {self.save_dir}')  # Otherwise => Exception.
+
+        probs, activations = self.sess.run([self.predictions, self.activations],
+                                           feed_dict={self.inputs: test_image, self.training: False})  # Predict.
+
+        predictions = np.argmax(probs, axis=1)
+        return predictions, probs, activations
+
 
 """
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True, reshape=False, validation_size=5000)
-x_train = mnist.train.images
-y_train = mnist.train.labels
-x_val = mnist.validation.images
-y_val = mnist.validation.labels
-x_test = mnist.test.images
-y_test = mnist.test.labels
-print(np.shape(x_test))
+# Load MNIST and, if omniglot_bool, Omniglot datasets.
+x_train, y_train, x_val, y_val, x_test, y_test = load_datasets(test_size=10, omniglot_bool=True, force=False)
 
+# Build model.
 tf.reset_default_graph()
 sess = tf.Session()
 net = MnistCNN(sess)
 
-#net.train_model(x_train, y_train, x_val, y_val, epochs=50, verbose=1)
+# Train model.
+net.train_model(x_train, y_train, x_val, y_val, epochs=1, verbose=1)
 
+# Test model.
 preds, _, activations = net.predict(x_test)
+
+# Evaluate with accuracy.
 accuracy = np.sum(np.argmax(y_test, 1) == preds)
-print(f'Test accuracy {accuracy/100} %')
+print(f'Test accuracy {accuracy/len(y_test)} %')
+
 print(np.shape(x_test))
 for i in range(len(activations)):
     print(np.shape(activations[i]))
 """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
